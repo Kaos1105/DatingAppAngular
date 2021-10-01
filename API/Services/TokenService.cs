b6f0d0288efb17e157ab146ea8dd0bf066a1e746
danhnt1105 <troll190599@gmail.com>
2021-10-01 12:16:33 +0700

    add authenticate JWT

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+  public class TokenService : ITokenService
+  {
+    private readonly SymmetricSecurityKey _key;
+    public TokenService(IConfiguration config)
+    {
+      _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+    }
+
+    public string CreateToken(AppUser user)
+    {
+      var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.NameId, user.UserName) };
+
+      var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      { Subject = new ClaimsIdentity(claim), Expires = DateTime.Now.AddDays(7), SigningCredentials = cred };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return tokenHandler.WriteToken(token);
+    }
+  }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -28,17 +23,16 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddApplicationsServices(_config);
 
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(_config.GetConnectionString("Default"));
-      });
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
       services.AddCors();
+
+      services.AddIdentityServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +50,8 @@ namespace API
       app.UseRouting();
 
       app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
